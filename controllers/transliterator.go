ae9591102b747b17046867b37f5ff875d272559c
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-03-22 05:20:05 -0400

    first commit

---
+++ controllers/transliterator.go
@@ -0,0 +1,46 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+var mGreek = map[string]string{"α": "a", "β": "b", "γ": "g", "δ": "d", "ε": "e", "ζ": "z", "η": "e", "θ": "th", "ι": "i", "κ": "k", "λ": "l", "μ": "m", "ν": "n", "ξ": "ks", "ο": "o", "π": "p", "ρ": "r", "σ": "s", "ς": "s", "τ": "t", "υ": "y", "φ": "ph", "χ": "ch", "ψ": "ps", "ω": "o"}
+var mHebrew = map[string]string{"א": "'", "ב": "b", "ג": "g", "ד": "d", "ה": "h", "ו": "w", "ז": "z", "ח": "ch", "ט": "t", "י": "y", "כ": "k", "ך": "k", "ל": "l", "מ": "m", "ם": "m", "נ": "n", "ן": "n", "ס": "s", "ע": "'", "פ": "p", "ף": "ph", "צ": "ts", "ץ": "ts", "ק": "q", "ר": "r", "שׁ": "s", "שׂ": "sh", "ת": "th"}
+
+func MainHandler(c echo.Context) error {
+	lang := c.QueryParam("language")
+	text := c.QueryParam("text")
+	if len(lang) == 0 || len(text) == 0 {
+		return c.String(http.StatusOK, "No language or text submitted.")
+	}
+	output := transliterate(lang, text)
+	return c.Render(http.StatusOK, "index.html", map[string]interface{}{
+		"output": output,
+	})
+}
+
+func transliterate(language string, text string) string {
+	var str string
+	if language == "Greek" {
+		for _, value := range text {
+			letter := string(value)
+			if letter == " " {
+				str += " "
+			} else {
+				str += mGreek[letter]
+			}
+		}
+	} else if language == "Hebrew" {
+		for _, value := range text {
+			letter := string(value)
+			if letter == " " {
+				str += " "
+			} else {
+				str += mHebrew[letter]
+			}
+		}
+	}
+	return str
+}
---
+++ server.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+type Text struct {
+	Body string `json:"body" form:"body" query:"body"`
+	Lang string `json:"lang" form:"lang" query:"lang"`
+}
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "method=${method}, uri=${uri}, status=${status}\n",
+	}))
+	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			// Extract the credentials from HTTP request header and perform a security
+			// check
+
+			// For invalid credentials
+			return echo.NewHTTPError(http.StatusUnauthorized, "Please provide valid credentials")
+
+			// For valid credentials call next
+			// return next(c)
+		}
+	})
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	e.GET("/users", func(c echo.Context) error {
+		return c.String(http.StatusOK, "/users")
+	})
+	e.GET("/users/:id", getUser)
+	e.GET("/hebrew", hebrew)
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func getUser(c echo.Context) error {
+	id := c.Param("id")
+	return c.String(http.StatusOK, id)
+}
+
+func hebrew(c echo.Context) error {
+	team := c.QueryParam("team")
+	member := c.QueryParam("member")
+	return c.String(http.StatusOK, "team:"+team+", member:"+member)
+}
+
+func NewText(c echo.Context) (err error) {
+	t := new(Text)
+	if err = c.Bind(t); err != nil {
+		return
+	}
+	return c.JSON(http.StatusOK, t)
+}
