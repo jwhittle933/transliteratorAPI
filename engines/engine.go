2060fefdd9ec50cdc41265b4a48917e210e77a2b
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-03-23 19:50:57 -0400

    language checker

--- engines/engine.go
+++ engines/engine.go
@@ -1,19 +1,9 @@
 package engine
 
-import (
-	"net/http"
-
-	"github.com/labstack/echo"
-)
-
-type Text struct {
-	Body string `json:"body" form:"body" query:"body"`
-	Lang string `json:"lang" form:"lang" query:"lang"`
-}
-
 var mGreek = map[string]string{"α": "a", "β": "b", "γ": "g", "δ": "d", "ε": "e", "ζ": "z", "η": "e", "θ": "th", "ι": "i", "κ": "k", "λ": "l", "μ": "m", "ν": "n", "ξ": "ks", "ο": "o", "π": "p", "ρ": "r", "σ": "s", "ς": "s", "τ": "t", "υ": "y", "φ": "ph", "χ": "ch", "ψ": "ps", "ω": "o"}
 var mHebrew = map[string]string{"א": "'", "ב": "b", "ג": "g", "ד": "d", "ה": "h", "ו": "w", "ז": "z", "ח": "ch", "ט": "t", "י": "y", "כ": "k", "ך": "k", "ל": "l", "מ": "m", "ם": "m", "נ": "n", "ן": "n", "ס": "s", "ע": "'", "פ": "p", "ף": "ph", "צ": "ts", "ץ": "ts", "ק": "q", "ר": "r", "שׁ": "s", "שׂ": "sh", "ת": "th"}
 
+// Transliterate func is the engine of the api
 func Transliterate(language string, text string) string {
 	var str string
 	if language == "Greek" {
@@ -39,10 +29,12 @@ func Transliterate(language string, text string) string {
 	return str
 }
 
-func NewText(c echo.Context) (err error) {
-	t := new(Text)
-	if err = c.Bind(t); err != nil {
-		return
-	}
-	return c.JSON(http.StatusOK, t)
+// LauguageCheck for determining language and text correspond
+func LauguageCheck(lang string, text string) bool {
+	return true
+}
+
+// OneOf used to link a char to a map
+func OneOf(text string) bool {
+	return false
 }
