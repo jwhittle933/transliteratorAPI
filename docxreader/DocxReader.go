db55dd7b7ba3a2d3cb810027e129fcc7815cc26e
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-03-30 14:30:44 -0400

    unzip file

--- docxreader/DocxReader.go
+++ docxreader/DocxReader.go
@@ -1,9 +1,10 @@
 package docxreader
 
 import (
-	"fmt"
-
-	"github.com/nguyenthenguyen/docx"
+	"archive/zip"
+	"io"
+	"os"
+	"path/filepath"
 )
 
 // NOTES :
@@ -11,23 +12,44 @@ import (
 
 // DocxReader for reading Word .docx files.
 func DocxReader() {
-	r, _ := docx.ReadDocxFile("../testfiles/hebrew.docx")
+	//
+}
 
-	docx := r.Editable()
-	fmt.Println(docx)
+// Unzip for exposing contexts of zip.
+func Unzip(pathToFile, saveLocation string) error {
+	reader, err := zip.OpenReader(pathToFile)
+	if err != nil {
+		return err
+	}
 
-	defer r.Close()
+	if err := os.MkdirAll(saveLocation, 0755); err != nil {
+		return err
+	}
 
-	// buffer := make([]byte, 9999)
+	for _, file := range reader.File {
+		path := filepath.Join(saveLocation, file.Name)
+		if file.FileInfo().IsDir() {
+			os.MkdirAll(path, file.Mode())
+			continue
+		}
 
-	// file, err := f.Open()
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// defer file.Close()
-	// int, err := file.ReadAt(buffer, 45)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Println(int)
+		fileReader, err := file.Open()
+		if err != nil {
+			return err
+		}
+		defer fileReader.Close()
+
+		targetFile, err := os.OpenFile(path, os.O_WRONLY|os.O_TRUNC, file.Mode())
+		if err != nil {
+			return err
+		}
+
+		defer targetFile.Close()
+
+		if _, err := io.Copy(targetFile, fileReader); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
