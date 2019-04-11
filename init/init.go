05e9a4e589460af843bfb160601dc10b7ebc99ed
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-04-11 08:47:27 -0400

    sql conn

--- init/init.go
+++ init/init.go
@@ -1,9 +1,19 @@
 package start
 
-import "github.com/labstack/echo"
+import (
+	"database/sql"
+
+	"github.com/labstack/echo"
+)
 
 // Init func for instantiating
-func Init() *echo.Echo {
+func Init() (*echo.Echo, *sql.DB, error) {
 	e := echo.New()
-	return e
+
+	conn, err := sql.Open("mysql", "user:password@/dbname")
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return e, conn, nil
 }
--- transliterator.go
+++ transliterator.go
@@ -14,7 +14,7 @@ import (
 )
 
 func main() {
-	e := start.Init()
+	e, conn, err := start.Init()
 
 	// MIDDLEWARE
 	mw.MiddleWare(e)
